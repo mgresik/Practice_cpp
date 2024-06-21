4e5f75b1971ec2fcac28331f478b2306ccbfce39
mgresik <mgresik@yandex.ru>
2024-06-21 17:41:31 +0300

    add registration

---
+++ registration.cpp
@@ -0,0 +1,55 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+ // Изменить путь прочтения и сохранения файлов, настроить запуск и компиляцию проекта
+
+bool IsLoggedIN() {
+  string username, password, un, pw;
+  cout << "enter username: ";
+  cin >> username;
+  cout << "enter password: ";
+  cin >> password;
+
+  ifstream read("data\\" + username + ".txt");
+  getline(read, un);
+  getline(read, pw);
+
+  if (un == username && pw == password) {
+    return true;
+  } else {
+    return false;
+  };
+}
+int main() {
+  int choice;
+  cout << "1: Registration\n2: Login\nChoice var: ";
+  cin >> choice;
+  if (choice == 1) {
+    string username, password;
+    cout << "Enter username: ";
+    cin >> username;
+    cout << "enter password: ";
+    cin >> password;
+
+    ofstream file;
+    file.open("data\\" + username + ".txt");
+    file << username << endl << password;
+    file.close();
+
+    main();
+  } else if (choice == 2) {
+    bool status = IsLoggedIN();
+    if (!status) {
+      cout << "Обманщик!" << endl;
+      system("Pause");
+      return 0;
+    }
+  } else {
+    cout << "Добро пожаловать!" << endl;
+    system("PAUSE");
+    return 1;
+  }
+}
