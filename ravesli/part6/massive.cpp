8edd1c7ba68a6ad3d3626e0d135e61c54ec500c3
mgresik <mgresik@yandex.ru>
2024-09-18 18:42:21 +0300

    sss

---
+++ ravesli/part6/massive.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+// namespace Animals
+// {   
+//     enum Animals
+//     {
+//         chicken,
+//         lion,
+//         giraffe,
+//         elephant,
+//         duck,
+//         snake,
+//         MAX_ANIMALS
+//     };
+// }
+
+int main()
+{
+    // double temperature[365] = {0.0};
+    // int legs[Animals::MAX_ANIMALS] = {2, 4, 4, 4, 2, 0};
+    // std::cout << "An elephant has " << legs[Animals::elephant] << " legs.\n";
+
+    int array[] = { 7, 5, 6, 4, 9, 8, 2, 1, 3 };
+    const int numVec = sizeof(array) / sizeof(array[0]);
+
+    return 0;
+}
