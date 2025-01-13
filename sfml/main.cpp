cf1002bc43c79cf703cdd1401d955020581f0688
mgresik <mgresik@yandex.ru>
2025-01-13 14:55:08 +0300

    длдл

---
+++ sfml/main.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+ 
+int main()
+{
+	// Объект, который, собственно, является главным окном приложения
+	RenderWindow window(VideoMode(400, 400), "SFML Works!");
+	
+	// Главный цикл приложения. Выполняется, пока открыто окно
+	while (window.isOpen())
+	{
+		// Обрабатываем очередь событий в цикле
+		Event event;
+		while (window.pollEvent(event))
+		{
+			// Пользователь нажал на «крестик» и хочет закрыть окно?
+			if (event.type == Event::Closed)
+				// тогда закрываем его
+				window.close();
+		}
+	window.clear(Color(90,100,250,0));
+    
+    // Отрисовка окна	
+	window.display();
+	}
+ 
+	return 0;
+}
